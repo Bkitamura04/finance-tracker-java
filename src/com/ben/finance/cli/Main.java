4c9fcec348a9dcc22b803035647719c53939dcbe
Ben Kitamura <ben.kitamura@gmail.com>
2025-07-01 09:52:18 -0700

    Set up folder structure and add Main.java stub

---
+++ src/com/ben/finance/cli/Main.java
@@ -0,0 +1,9 @@
+package com.ben.finance.cli;
+
+public class Main
+{
+     public static void main(String[] args)
+     {
+          System.out.println("Finance Tracker CLI starting up...");
+     }
+}
